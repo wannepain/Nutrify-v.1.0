03db6b83b111e38aa85120681e909ad7fe177756
Marek Wannepain <marekwannepain@gmail.com>
2024-04-03 06:53:10 +0200

    work on styling, saving tokens as cookies and authentication

--- frontend/React/src/components/App.jsx
+++ frontend/React/src/components/App.jsx
@@ -6,11 +6,17 @@ import { useState } from "react";
 function App() {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
-  return (
-    <div>
-      <LogIn />
-    </div>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  if (isLoggedIn){
+    return <h1>Welcome</h1>
+  }else {
+    return (
+      <div>
+        <LogIn setLog={setIsLoggedIn}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- frontend/React/src/components/LogIn.jsx
+++ frontend/React/src/components/LogIn.jsx
@@ -4,22 +4,60 @@ import axios from 'axios';
 import "./../../public/LogIn.css"
 
 function LogIn(props) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  // const [username, setUsername] = useState('');
+  // const [password, setPassword] = useState('');
+  const [values, setValues] = useState({ username: "", password: "" });
+  const [error, setError] = useState(false)
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    name === "Username"? setValues({ username: value, password: values.password }) : setValues({ username: values.username, password: value });
+    // setValues({ ...values, [name]: value });
+  }
+  // Function to get cookie value by name
+    function getCookie(name) {
+      const cookies = document.cookie.split(';');
+      for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.trim().split('=');
+        if (cookieName === name) {
+          return decodeURIComponent(cookieValue);
+        }
+      }
+      return null;
+    }
   const handleLogin = async (event) => {
     event.preventDefault();
-    try {
-      // Perform login request to the backend using Axios
-      const response = await axios.post('http://localhost:3000/login/local', {
-        username: username,
-        password: password
-      });
-      console.log(response);
-      // Handle successful login, e.g., redirect to another page
-      // or store authentication token in local storage
-    } catch (error) {
-      console.error('Login error:', error);
+    const {username, password} = values
+    if(username === "" || password === ""){
+      setError("Username and password mustn't be empty");
+    }else{
+      try {
+        // Perform login request to the backend using Axios
+        const response = await axios.post('http://localhost:3000/login/local', {
+          username: username,
+          password: password
+        });
+        console.log(response);
+        // Handle successful login, e.g., redirect to another page
+        // or store authentication token in local storage
+        if (response.status === 200){
+          const token = response.data.token;
+          const [header, payload, signature] = token.split(".");
+          // Set up the cookies
+          document.cookie = `jwtHeader=${header}; Secure; SameSite=Strict`;
+          document.cookie = `jwtPayload=${payload}; Secure; SameSite=Strict`;
+          document.cookie = `jwtSignature=${signature}; Secure; HttpOnly; SameSite=Strict`;
+          props.setLog(true);
+        } 
+      } catch (error) {
+        console.error('Login error:', error);
+        const err = error.response.status
+        if(err === 401){
+          setError("Incorrect password or username");
+        } else{
+          setError("Server error, please try again later")
+        }
+      }
     }
   };
 
@@ -27,21 +65,26 @@ function LogIn(props) {
     <div>
       <h1>Log In</h1>
       <form id='logInForm'>
-        <input
-          name='Username'
-          className='topInput'
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          className='bottomInput'
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <div className='inputContainer'>
+          <input
+            name='Username'
+            className='topInput'
+            type="text"
+            placeholder="Username"
+            value={values.username}
+            onChange={handleChange}
+            autoComplete='username'
+          />
+          <input
+            className='bottomInput'
+            type="password"
+            placeholder="Password"
+            value={values.password}
+            onChange={handleChange}
+            autoComplete='current-password'
+          />
+        </div>
+        {!error? null : <p id='error'>{error}</p>}
         <button className="formBtn" onClick={handleLogin}>Login</button>
       </form>
     </div>
